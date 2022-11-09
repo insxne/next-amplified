3576c2b27c0e1b3151461b2c12fc18ab9af0db0c
Mario Negrete <txus2k@gmail.com>
2022-11-09 15:44:34 -0300

    test show host

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-function Home({ name }) {
-  return <>Hola {name}</>
+function Home({ name, host }) {
+  return <>Hola {name} ({host})</>
 }
 
 export async function getServerSideProps(context) {
+  const hostname = context.req.headers.host
+
   return {
-    props: { name: 'Mario' }
+    props: { name: 'Mario', host: hostname }
   }
 }
 
